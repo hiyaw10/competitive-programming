5c583922bd96cbcd59f9c69bb1698a540e4107ab
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-10-04 11:14:05 +0300

    Time: 29 ms (97.07%), Space: 13.9 MB (55.24%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0:
+            return(0)
+        if n==1:
+            return(1)
+        else:
+            tot = 0
+            f0 = 0
+            f1 = 1
+            while n > 1:
+                tot = f0 + f1
+                f0 = f1
+                f1 = tot
+                n-=1
+
+            return(tot)
