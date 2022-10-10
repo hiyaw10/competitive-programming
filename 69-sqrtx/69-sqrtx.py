3bdb856668338920710cb1069755ca8629a2f62a
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-10-10 03:35:19 +0300

    Time: 41 ms (90.90%), Space: 13.8 MB (95.62%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,9 +1,6 @@
 class Solution:
+    import math
     def mySqrt(self, x: int) -> int:
-        for i in range(0,x+1):
-            if i*i>x:
-                return i-1
-                break
-            elif i*i==x:
-                return i
-                break
+        y = math.sqrt(x)
+        y = int(y)
+        return y
