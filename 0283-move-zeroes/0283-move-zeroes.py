d3e5a8ddfd7741a71fbf006673f3d8e658f7464a
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-10-20 10:09:24 +0300

    Time: 180 ms (89.39%), Space: 15.6 MB (17.04%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,12 @@
+'''
+we are looking for an array with zeroes on the most left while the order of the remaining array is maintained.
+in order to do that we need two pointers in order to not take extra memory space, so one pointer to scan through the List, while the other to do some more switches, it has a time complexitiy of O(n).
+'''
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[r], nums[l] = nums[l], nums[r]
+                l += 1
+        return nums
