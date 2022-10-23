6ce30d3eab19e171840a2b7843d8fc0d95052888
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-10-23 09:34:51 +0300

    Time: 76 ms (34.31%), Space: 13.8 MB (99.21%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,20 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        pointer = len(nums1) - 1
-        m = m - 1
-        n = n - 1
-        
-        while m >= 0 and n >= 0:
-            if nums1[m] >= nums2[n]:
-                nums1[pointer] = nums1[m]
+        while n > 0 and m > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
                 m -= 1
             else:
-                nums1[pointer] = nums2[n]
+                nums1[m+n-1] = nums2[n-1]
                 n -= 1
+        while n > 0:
+            nums1[m+n-1] = nums2[n-1]
+            n -= 1
                 
-            pointer -= 1
-            
-        if n >= 0:
-            nums1[:n+1] = nums2[:n+1]
-        
