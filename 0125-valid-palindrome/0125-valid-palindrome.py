137e1174a491f73fecd6c15d090d52f1b1b65a9c
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-10-23 13:03:33 +0300

    Time: 104 ms (36.30%), Space: 14.4 MB (85.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,9 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         newStr = ""
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
+        for c in s: #for every character in out input
+            if c.isalnum(): #if that character is an alpha numeric character
+                newStr += c.lower() #We just push it on to newStr(but lower).
+        return newStr == newStr[::-1] #after pushing every single character onto our new string, we return true if its reversed self is the same as itself.
             
         
