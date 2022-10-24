6189d6dab94791abeb27d07415b38499e75ea79d
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-10-24 12:02:30 +0300

    Time: 48 ms (71.24%), Space: 13.8 MB (72.12%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,11 +5,12 @@ class Solution:
         stack = []
         Map = {")":"(", "}":"{", "]":"["}
         for c in s:
-            if c in Map: #closing parenthesis
+            if c in Map:
                 if stack and stack[-1] == Map[c]:
-                    stack.pop() #continue going
+                    stack.pop()
                 else:
                     return False
             else:
                 stack.append(c)
         return True if not stack else False
+                
