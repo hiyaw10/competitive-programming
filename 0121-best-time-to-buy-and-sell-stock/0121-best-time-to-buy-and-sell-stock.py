9e650e0ff33296c494bfae3126e3d02b6273bf85
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-10-24 22:58:22 +0300

    Time: 3075 ms (8.65%), Space: 25.1 MB (7.07%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxP = 0
-        l , r = 0, 1
+        l, r = 0, 1
         while r < len(prices):
-            #we need to buy it when it is valued less[ when the right pointer is much lesser]
-            if prices[l] < prices[r]:
-                Profit = prices[r] - prices[l]
-                maxP = max(maxP, Profit)
+            if prices[r] > prices[l]:
+                difference = prices[r] - prices[l]
+                maxP = max(difference, maxP)
             else:
-                l=r    
+                l = r
             r += 1
         return maxP
