2bafe3b9ccc6f820019ccbb864925a998cb8e1fd
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-12-22 14:04:37 +0300

    Time: 61 ms (94.63%), Space: 15.1 MB (53.15%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        d = {}
-        for i, j in enumerate(nums):
-            r = target - j
-            if r in d: return [d[r], i]
-            d[j] = i
+        """
+        we need a hashmap
+        we check for the difference of the target and the current number in the library
+        then we are good to go
+        """
+        hashMap = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            else:
+                hashMap[n] = i
