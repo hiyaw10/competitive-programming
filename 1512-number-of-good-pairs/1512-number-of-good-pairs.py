08a8666441472c0a60b34e3364b2ff22a437fa3f
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-12-23 11:02:46 +0300

    Time: 65 ms (42.86%), Space: 13.9 MB (10.78%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,28 +1,10 @@
 class Solution:
-    
-    # search for duplicate numbers
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        res = 0
         
-        # number of good pairs
-        repeat = {}
-        num = 0
+        for n in counter.values():
+            if n > 1:
+                res += (n-1) * (n)// 2
+        return res
         
-        # for every element in nums
-        for v in nums:
-            
-            # number of repeated digits
-            if v in repeat:
-                
-                # count number of pairs based on duplicate values
-                if repeat[v] == 1:
-                    num += 1
-                else:
-                    num += repeat[v]
-                
-                # increment the number of counts
-                repeat[v] += 1
-            # number has not been seen before
-            else:
-                repeat[v] = 1
-        # return
-        return num
