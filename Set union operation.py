5e4d6ce4bc3381d2d593cd7ff9ddd27523863c7a
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2022-12-25 11:16:29 +0300

    Create Set union operation.py

---
+++ Set union operation.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+N = int(input())
+E = list(map(int, input().split()))
+G = int(input())
+F = list(map(int, input().split()))
+count = 0
+for i in E:
+    if i in F:
+        continue
+    else:
+        count += 1
+print (count + len(set(F)))
