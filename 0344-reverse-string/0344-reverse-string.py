735005f58fcdb83ce25f069a582f985ace62880b
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-01-11 19:17:59 +0300

    Time: 199 ms (91.62%), Space: 18.4 MB (40.97%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        stack = []
-        for i in s:
-            stack.append(i)
-        i = 0
-        while stack:
-            s[i] = stack.pop()
-            i += 1
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+            
