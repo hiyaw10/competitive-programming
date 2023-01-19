242b78a49fd0474128562658a6e3c5ba0575c56e
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-01-19 09:08:25 +0300

    Time: 39 ms (76.80%), Space: 14.1 MB (25.70%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,13 +1,13 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        k = k-1
-        index = 0
-        li = [i for i in range(1,n+1)]
-        def cal(k,index):
-            if len(li) == 1:
-                return li[0]
-            index = (index + k)% len(li)
-            del li[index]
-            return cal(k,index)
-        return cal(k,index)
+        
+        nums = list(range(1, n+1))
+        print(nums)
+        
+        i = 0
+        while len(nums) > 1:
+            i = (i + k - 1) % len(nums)
+            nums.pop(i)
+        return sum(nums)
+            
         
