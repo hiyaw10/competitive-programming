f34907b81b7ad9f8635d0cf8680e7d28ae893080
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-01-20 18:22:03 +0300

    Time: 39 ms (98.75%), Space: 14.1 MB (14.05%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,73 +1,11 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hashmap1 = {}
-        hashmap2 = {}
-        res = []
-        for num in nums1:
-            if num in hashmap1:
-                hashmap1[num] += 1
-            else:
-                hashmap1[num] = 1
-        for num in nums2:
-            if num in hashmap2:
-                hashmap2[num] += 1
-            else:
-                hashmap2[num] = 1
-        #we have created a hashmap of their frequency
-        for key in hashmap1:
-            if key in hashmap2:
-                freq = min(hashmap1[key],hashmap2[key])
-                for i in range(freq):
-                    res.append(key)
-            else:
-                continue
-        return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # nums1.sort()
-        # nums2.sort()
-        # l, r = 0, 0
-        # res = []
-        # while l < len(nums1) and r < len(nums2):
-        #     if nums1[l] == nums2[r]:
-        #         res.append(nums1[l])
-        #         l += 1
-        #         r += 1
-        #     elif nums1[l] > nums2[r]:
-        #         r += 1
-        #     else:
-        #         l += 1
-        # return res
+        hashmap1 = Counter(nums1)
+        hashmap2 = Counter(nums2)
+        array = []
+        for i in hashmap1:
+            if i in hashmap2:
+                freq = min(hashmap2[i], hashmap1[i])
+                for _ in range(freq):
+                    array.append(i)
+        return array
