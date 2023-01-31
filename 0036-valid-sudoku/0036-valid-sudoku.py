2b5361df01b7aacce05c5f4c99c152cc6a632753
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-01-31 08:21:10 +0300

    Time: 86 ms (98.43%), Space: 13.9 MB (76.43%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,15 +1,19 @@
-
 class Solution:
-    def isValidSudoku(self, board):
-        for row in chain(board, zip(*board)):
-            cand = [i for i in row if i != "."]
-            if len(set(cand)) != len(cand): return False
-            
-        for x, y in product([1,4,7],[1,4,7]):
-            cand = [board[x+i][y+j] for i,j in product([-1,0,1],[-1,0,1])]
-            cand = [i for i in cand if i != "."]
-            if len(set(cand)) != len(cand): return False
-
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        dic_list1 = [set() for _ in range(9)]
+        dic_list2 = [set() for _ in range(9)]
+        dic_list3 = [[set() for _ in range(3)] for _ in range(3)]
+        for i in range(9):
+            for j in range(9):
+                if not board[i][j].isnumeric():
+                    continue
+                y = i // 3
+                x = j // 3
+                num = board[i][j]
+                if (num in dic_list1[i]) or (num in dic_list2[j]) or (num in dic_list3[x][y]):
+                    return False
+                else:
+                    dic_list1[i].add(num)
+                    dic_list2[j].add(num)
+                    dic_list3[x][y].add(num)
         return True
-        
-          
