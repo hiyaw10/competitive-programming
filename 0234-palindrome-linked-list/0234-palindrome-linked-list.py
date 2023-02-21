3d4b76f50077ef0c97a33600532b6922eaac6f47
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-02-21 15:32:57 +0300

    Time: 862 ms (42.48%), Space: 46.8 MB (34.57%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,27 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        fast = head
-        slow = head
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-        # slow is the second half of head
-        # reverse slow:
-        rev_slow = self.rev_ll(slow)
-        # compare rev_slow to the first half of head, node by node
-        while rev_slow:
-            if rev_slow.val != head.val:
-                return False
-            rev_slow = rev_slow.next
-            head = head.next
-        return True
+        arr = []
         
-    # reverse a linked list
-    def rev_ll(self, head):
-        prev = None
-        while head:
-            cur = head
-            head = head.next
-            cur.next = prev
-            prev = cur
-        return cur
+        itr = head
+        while itr:
+            arr.append(itr.val)
+            itr = itr.next
+        return arr == arr[::-1]
+        
