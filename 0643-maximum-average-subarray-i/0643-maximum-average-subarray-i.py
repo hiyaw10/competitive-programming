d1465fe3439168b2d30178de812f453900346279
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-02-23 21:08:21 +0300

    Time: 1324 ms (42.75%), Space: 26.2 MB (40.28%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -4,9 +4,10 @@ class Solution:
       
         curr_sum = sum(nums[:k])
         Sum = curr_sum
-        
+        left = 0
         for right in range(k, len(nums)):
-            curr_sum += nums[right] - nums[right-k]
+            curr_sum += nums[right] - nums[left]
+            left += 1
             Sum = max(curr_sum, Sum)
         return Sum/k
             
