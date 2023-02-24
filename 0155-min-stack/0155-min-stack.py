8f4b13d87724f60844fd84793e65c11ac3c054c7
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-02-24 10:09:59 +0300

    Time: 66 ms (53.29%), Space: 18 MB (65.45%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,21 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            val = min(val, self.minStack[-1])
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.minStack:
+            return self.minStack[-1]
