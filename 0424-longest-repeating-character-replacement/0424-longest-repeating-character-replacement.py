0f3e79a275a00b0055a083a564e9c8372bb4d729
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-02-26 18:07:38 +0300

    Time: 180 ms (41.78%), Space: 13.9 MB (51.35%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,25 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
         l = 0
-        c_frequency = {}
-        longest_str_len = 0
-        for r in range(len(s)):
-            
-            if not s[r] in c_frequency:
-                c_frequency[s[r]] = 0
-            c_frequency[s[r]] += 1
-            
-            
-            cells_count = r - l + 1
-            if cells_count - max(c_frequency.values()) <= k:
-                longest_str_len = max(longest_str_len, cells_count)
-                
-            else:
-                c_frequency[s[l]] -= 1
-                if not c_frequency[s[l]]:
-                    c_frequency.pop(s[l])
-                l += 1
+        res = 0
+        hashmap = {}
         
-        return longest_str_len
-		
+        for r in range(len(s)):
+            hashmap[s[r]] = 1 + hashmap.get(s[r], 0)
+            while (r-l + 1) - max(hashmap.values()) > k:
+                hashmap[s[l]] -= 1
+                l += 1
+            res = max(r-l+1, res)
+        return res
