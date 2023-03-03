2a485ee014f3fe2b37d4bba5d7f04ff7ecb55229
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-03-03 11:05:28 +0300

    Time: 857 ms (88.05%), Space: 28 MB (77.02%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,15 +1,12 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        result = [0] * (n+1)
-        #mark the range with the deltas (head and tail)
-        for booking in bookings:
-            #start
-            result[booking[0]-1] += booking[2]
-            #end
-            result[booking[1]] -= booking[2]
-        #cumulative sum processing
-        tmp = 0
-        for i in range(n):
-            tmp += result[i]
-            result[i] = tmp
-        return result[:n]
+        ans = [0] * n
+        for first, last, seat in bookings:
+            
+            if first <= n: ans[first-1] += seat
+            if last < n: ans[last] -= seat
+        Sum = 0
+        for i in range(len(ans)):
+            Sum += ans[i]
+            ans[i] = Sum
+        return ans
