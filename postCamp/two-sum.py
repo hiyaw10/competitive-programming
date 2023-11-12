875e6b1ada2bf6724831ab6020f114f037b6dfa0
hiyaw10 <114156496+hiyaw10@users.noreply.github.com>
2023-11-12 13:23:39 +0300

    Added solution for Two Sum

---
+++ postCamp/two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        
+        for i, n in enumerate(nums):
+            diff = target - nums[i]
+            
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[n] = i
+            
+        
